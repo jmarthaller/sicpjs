2fbec5c9916d07b1233c3313806892bcc10ea2ec
Jacob Marthaller <marthaller.jr@gmail.com>
2023-12-16 10:43:19 -0800

    Day 15 (1 day behind)

--- main.rb
+++ main.rb
@@ -1393,3 +1393,18 @@ end
 # data = File.open("day14.txt").readlines(chomp: true)
 # data = cycles(data, 1000000000)
 # puts north_load(data)
+
+# Day 15
+input = File.read("day15.txt").tr("\r\n", '').split(',')
+hash = ->(l) { l.bytes.reduce(0) { (_1 + _2) * 17 % 256 } }
+
+boxes = {}
+input.map do |l|
+    lbl, op = l.split(/[=-]/)
+    box = boxes[1 + hash[lbl]] ||= []
+    i = box.index { _1[0] == lbl } || box.size
+    op ? box[i] = [lbl, op.to_i] : box.delete_at(i)
+end
+
+puts "Part 1: #{input.sum(&hash)}"
+puts "Part 2: #{boxes.sum { |(i, b)| i * b.map.with_index.sum { |l, j| -~j * l[1] } }}"
